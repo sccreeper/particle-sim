b4d3eec3df039aad92db43a706271377c7c03a80
Oscar Peace <oscar@oscarcp.net>
2026-08-07 14:40:08 +0100

    meta: initial commit

---
+++ sim/main.cpp
@@ -0,0 +1,30 @@
+#include <raylib.h>
+#include <iostream>
+#include <cstdint>
+
+int main() {
+
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+
+    InitWindow(400, 400, "Particle Sim");
+
+    std::cout << "HELLPPP" << std::endl;
+
+    int64_t counter = 0;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(BLUE);
+        DrawRectangle(10, 20, 30, 40, RED);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    
+    return 0;
+}
